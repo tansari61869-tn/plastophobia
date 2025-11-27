17933ae4399f494c3e9c9aa92795133c85d4c01b
Thameem Ansari.S <tansari61869@gmail.com>
2025-11-27 17:55:34 +0000

    Commit changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  if (!preloader) return;
+
+  // little delay for smooth feel
+  setTimeout(() => {
+    preloader.classList.add("hidden");
+  }, 600);
+});
 
